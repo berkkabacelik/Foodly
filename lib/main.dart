5ce23568ae68a8aa8740cd4adb1c903b6c037688
berkkabacelik <berkkabacelik@gmail.com>
2025-03-19 11:04:45 +0300

    -

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:foodly/auth/login_or_register.dart';
+import 'package:foodly/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+      )
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+ 
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginOrRegister(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+   
+    );
+  }
+}
+
+
+  
+
